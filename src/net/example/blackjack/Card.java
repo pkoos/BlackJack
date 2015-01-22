4cb2e26e98ddf85db4044073f5f23f5d1788a1a6
pkoos <pk5298@gmail.com>
2015-01-21 19:36:56 -0800

    Changed Card code to remove switch statement

--- src/net/example/blackjack/Card.java
+++ src/net/example/blackjack/Card.java
@@ -2,57 +2,13 @@ package net.example.blackjack;
 
 public class Card {
 	final Rank rank;
-	private int cardVal;
+	private final int cardVal;
 
 	public Card(Rank r) {
 		rank = r;
-		this.setCardValue();
+		cardVal = Math.min(10, rank.ordinal()+1);
 	}
-	
-	public void setCardValue() {
-		switch(rank) { 
-		case Ace: 
-			cardVal = 1;
-			break;
-		case Two:
-			cardVal = 2;
-			break;
-		case Three:
-			cardVal = 3;
-			break;
-		case Four:
-			cardVal = 4;
-			break;
-		case Five: 
-			cardVal = 5;
-			break;
-		case Six:
-			cardVal = 6;
-			break;
-		case Seven:
-			cardVal = 7;
-			break;
-		case Eight:
-			cardVal = 8;
-			break;
-		case Nine:
-			cardVal = 9;
-			break;
-		case Ten:
-			cardVal = 10;
-			break;
-		case Jack:
-			cardVal = 10;
-			break;
-		case Queen:
-			cardVal = 10;
-			break;
-		case King:
-			cardVal = 10;
-			break;
-		}
-	}
-	
+
 	public int getCardValue() {
 		return cardVal;
 	}
