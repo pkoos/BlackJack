fded8e92ba1dffbece6241ea0f95a66e4d8fad13
pkoos <pk5298@gmail.com>
2015-01-29 21:11:41 -0800

    finished behavioral test method shuffle_maintains_four_of_each_card_rank()

--- src/test/java/net/example/blackjack/DeckTest.java
+++ src/test/java/net/example/blackjack/DeckTest.java
@@ -82,21 +82,60 @@ public class DeckTest {
 		Random r = new Random();
 		r.setSeed(12L);
 		Deck d = new Deck(r);
-		
-		d.shuffle();
-		
-		assertEquals(Rank.Nine,d.getTopCard().rank);
-		assertEquals(Rank.Three,d.getTopCard().rank);
-		assertEquals(Rank.Queen,d.getTopCard().rank);
-		assertEquals(Rank.King,d.getTopCard().rank);
-		assertEquals(Rank.Eight,d.getTopCard().rank);
 
-		
+		d.shuffle();
+
+		assertEquals(Rank.Nine, d.getTopCard().rank);
+		assertEquals(Rank.Three, d.getTopCard().rank);
+		assertEquals(Rank.Queen, d.getTopCard().rank);
+		assertEquals(Rank.King, d.getTopCard().rank);
+		assertEquals(Rank.Eight, d.getTopCard().rank);
 	}
-	
+
 	@Test
 	public void shuffle_maintains_four_of_each_card_rank() {
-		
-		fail("TODO");
+		Random r = new Random();
+		r.setSeed(12L);
+		Deck d = new Deck(r);
+		int countAce = 0;
+		int countTwo = 0;
+		int countThree = 0;
+		int countFour = 0;
+		int countFive = 0;
+		int countSix = 0;
+		int countSeven = 0;
+		int countEight = 0;
+		int countNine = 0;
+		int countTen = 0;
+		int countJack = 0;
+		int countQueen = 0;
+		int countKing = 0;
+
+		d.shuffle();
+
+		assertEquals(4, new DeckTest().countTheCards(Rank.Ace, d, countAce));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Two, d, countTwo));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Three, d, countThree));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Four, d, countFour));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Five, d, countFive));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Six, d, countSix));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Seven, d, countSeven));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Eight, d, countEight));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Nine, d, countNine));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Ten, d, countTen));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Jack, d, countJack));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Queen, d, countQueen));
+		assertEquals(4, new DeckTest().countTheCards(Rank.Queen, d, countKing));
+
 	}
+
+	public int countTheCards(Rank cardRank, Deck deck, int counter) {
+		for (Card card : deck.cards) {
+			if (card.rank == cardRank) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+
 }
