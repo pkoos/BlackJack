fce78b5e2eb35eebae39d6c4fc82792dd7e72376
pkoos <pk5298@gmail.com>
2015-02-02 19:02:53 -0800

    added Hand methods

---
+++ src/main/java/net/example/blackjack/Hand.java
@@ -0,0 +1,22 @@
+package net.example.blackjack;
+
+public class Hand {
+	Card[] hand;
+	private int handScore;
+
+	public Hand(Player player, int initialCards) {
+		this.fillHand(initialCards);
+	}
+
+	public void fillHand(int numCards) {
+
+	}
+
+	public void calculateAceScore() {
+
+	}
+
+	public void setHandScore() {
+		
+	}
+}
